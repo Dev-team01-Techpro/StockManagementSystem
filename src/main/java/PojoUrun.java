ec8ca5564fd5ffe63fd9c3765f9455c78ecfdb34
Ata <ahmetsedatata11@gmail.com>
2023-09-16 00:55:21 +0200

    PojoUrun Class

---
+++ src/main/java/PojoUrun.java
@@ -0,0 +1,58 @@
+public class PojoUrun {
+
+    private int id;
+    private String urunIsmi;
+    private String uretici;
+    private int miktar = 0;
+    private String birim;
+    private String raf = "-" ;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrunIsmi() {
+        return urunIsmi;
+    }
+
+    public void setUrunIsmi(String urunIsmi) {
+        this.urunIsmi = urunIsmi;
+    }
+
+    public String getUretici() {
+        return uretici;
+    }
+
+    public void setUretici(String uretici) {
+        this.uretici = uretici;
+    }
+
+    public int getMiktar() {
+        return miktar;
+    }
+
+    public void setMiktar(int miktar) {
+        this.miktar = miktar;
+    }
+
+    public String getBirim() {
+        return birim;
+    }
+
+    public void setBirim(String birim) {
+        this.birim = birim;
+    }
+
+    public String getRaf() {
+        return raf;
+    }
+
+    public void setRaf(String raf) {
+        this.raf = raf;
+    }
+}
