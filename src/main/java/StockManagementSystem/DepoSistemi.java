a881164f80cdd737fc11f0d11b15edadba54addd
Emrullah Saglam <demdemirey@gmail.com>
2023-09-17 13:48:08 +0200

    depo Sistemi class ve icerigi olusturuldu

---
+++ src/main/java/StockManagementSystem/DepoSistemi.java
@@ -0,0 +1,20 @@
+package StockManagementSystem;
+
+public class DepoSistemi {
+
+    public static void urunTanimlama(){
+
+    }
+    public static void urunListele(){
+
+    }
+    public static void urunGirisi(){
+
+    }
+    public static void urunuRafaKoy(){
+
+    }
+    public static void urunCikisi(){
+
+    }
+}
