be92c4edcddad087d092c021fd604a35bf6ce6e7
ata324 <mavican324@gmail.com>
2023-09-19 23:13:45 +0200

    Cahit

--- src/main/java/DepoSistemi.java
+++ src/main/java/DepoSistemi.java
@@ -1,11 +1,60 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class DepoSistemi {
+    Scanner scan=new Scanner(System.in);
+    static int urunId = 0;
 
-    public static void urunTanimlama() {
-        //Bu metot, kullanıcıdan ürün adı, üretici ve birim bilgilerini alarak yeni bir ürün tanımlayacaktır.
-        // Her ürün için bir id oluşturulacak ve bu id ile birlikte ürün bilgileri bir Map veri yapısında saklanacaktır.
-        // Ürünün miktarı başlangıçta 0 olacak ve raf bilgisi "-" olarak atanacaktır.
+    public HashMap urunTanimlama(ArrayList<HashMap<Integer,PojoUrun>> list) {
 
-    }
+
+            System.out.println("Lutfen urun ismi giriniz.");
+            String urunIsmi=scan.nextLine();
+            System.out.println("Lutfen uretici giriniz.");
+            String uretici=scan.nextLine();
+            System.out.println("Lutfen birim giriniz.");
+            String birim=scan.nextLine();
+
+            HashMap<Integer, PojoUrun> map = new HashMap<>();
+
+            PojoUrun urun = new PojoUrun();
+
+            int id =idTanimlama();
+
+
+
+            urun.setUrunIsmi(urunIsmi);
+            urun.setUretici(uretici);
+            urun.setBirim(birim);
+            urun.setId(id);
+
+
+            map.put(id, urun);
+            list.add(map);
+
+            return map;
+        }
+
+
+        public  int idTanimlama() {
+
+
+            if (urunId == 0) {
+
+                urunId = 1;
+
+            } else {
+
+                urunId += 1;
+
+            }
+            return urunId;
+
+
+        }
+
+
 
     public static void urunListele() {
         //Bu metot, tanımlanan ürünleri listelemek için kullanılacaktır.
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,13 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<HashMap<Integer, PojoUrun>> list = new ArrayList<>();
+        DepoSistemi depo = new DepoSistemi();
+
+
+
+        depo.urunTanimlama(list);
+
     }
 
 }
