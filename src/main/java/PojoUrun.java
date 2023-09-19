4385727d511b1919caddde368f3d990e414d81aa
Ata <ahmetsedatata11@gmail.com>
2023-09-19 22:28:48 +0200

    pushPojoUrun

--- src/main/java/PojoUrun.java
+++ src/main/java/PojoUrun.java
@@ -9,50 +9,62 @@ public class PojoUrun {
 
 
     public int getId() {
+
         return id;
     }
 
     public void setId(int id) {
+
         this.id = id;
     }
 
     public String getUrunIsmi() {
+
         return urunIsmi;
     }
 
     public void setUrunIsmi(String urunIsmi) {
+
         this.urunIsmi = urunIsmi;
     }
 
     public String getUretici() {
+
         return uretici;
     }
 
     public void setUretici(String uretici) {
+
         this.uretici = uretici;
     }
 
     public int getMiktar() {
+
         return miktar;
     }
 
     public void setMiktar(int miktar) {
+
         this.miktar = miktar;
     }
 
     public String getBirim() {
+
         return birim;
     }
 
     public void setBirim(String birim) {
+
         this.birim = birim;
     }
 
     public String getRaf() {
+
         return raf;
     }
 
     public void setRaf(String raf) {
+
         this.raf = raf;
     }
 }
